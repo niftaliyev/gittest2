abe877beb33ddc3103b96c79e95e0101e90845f4
Kamran <kamran.niftaliyev@eilink.az>
2024-02-14 20:00:07 +0400

    instruction to git repository add

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 ﻿Console.WriteLine("git test, 2!");
+
+
+/*
+ * 
+ * 
+ * echo "# gittest2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin -------
+git push -u origin main
+ */
